62f31fb1cdca1d6ceea0dc4e08e870d24421aa57
Diego Velez <diego.velez.dev@gmail.com>
2022-05-30 22:02:27 -0700

    Do not print command output when unnecesary and change print of programs installed

--- .config/setup-scripts/AppInstaller.py
+++ .config/setup-scripts/AppInstaller.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from Colorize import with_color, Color
 
 DNF_PROGRAMS = [
@@ -19,31 +20,34 @@ def check() -> None:
     # Check which programs are installed and remove them from the list
     programs_installed = list()
     for program in DNF_PROGRAMS:
-        return_code = os.system(f'which {program}')
+        return_code = subprocess.call(['which', program], stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
 
         if return_code == 0:
-            print(with_color(f'{program} already installed\n', Color.Green))
             programs_installed.append(program)
+
+    print(with_color(f'{", ".join(programs_installed)} already installed\n', Color.Green))
 
     for program in programs_installed:
         DNF_PROGRAMS.remove(program)
 
     if len(DNF_PROGRAMS) > 0:
         programs = ' '.join(DNF_PROGRAMS)
-        dnf_program_install_command = f'sudo dnf install {programs}'
+        dnf_program_install_command = ['sudo dnf install', programs]
 
-        os.system(dnf_program_install_command)
+        subprocess.call(dnf_program_install_command)
 
         print()
 
     # Check if powerline is installed
     program = 'powerline-shell'
-    if os.system(f'which {program}') == 0:
+    command = ['which', program]
+    return_code = subprocess.call(command, stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
+    if return_code == 0:
         print(with_color(f'{program} already installed\n', Color.Green))
     else:
-        powerline_install_command = f'sudo pip install {program}'
+        powerline_install_command = ['sudo pip install', program]
 
-        os.system(powerline_install_command)
+        subprocess.call(powerline_install_command)
 
         print()
 
