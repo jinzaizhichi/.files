bebd37d48149eb11fdedd8bffad03fba65b5df55
Diego Vélez <diego.velez.dev@gmail.com>
2024-09-13 18:43:24 -0400

    Use same keymap to toggle waybar, reduce window border radius, add window rules, add another screensaver, and change waybar script in order to toggle instead of using arguments

--- .config/hypr/screensaver.sh
+++ .config/hypr/screensaver.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 
 # Choose a screensaver ramdomly
-screensaver_chosen=$(($RANDOM % 3))
+screensaver_chosen=$(($RANDOM % 4))
 
 if [[ "$screensaver_chosen" == "0" ]]; then
 	# Sleeping in order to wait for the terminal to be fullscreen, then running pipes
-	screensaver="sleep 0.1 && /home/dvt/.local/bin/pipes.sh -r 0 -R -p 5"
+	screensaver="sleep 0.4 && ~/.local/bin/pipes.sh -r 0 -R -p 5"
 elif [[ "$screensaver_chosen" == "1" ]]; then
 	screensaver="asciiquarium"
+elif [[ "$screensaver_chosen" == "2" ]]; then
+	screensaver="sleep 0.4 && ~/repo_clones/lavat/lavat -k yellow -c red -s 4 -r 4"
 else
 	screensaver="cmatrix -r"
 fi
