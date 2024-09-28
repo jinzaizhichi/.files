520f0e927330593a19293b22c1c39ae29fca5000
Diego Vélez <diego.velez.dev@gmail.com>
2024-09-28 08:10:44 -0400

    Use Kitty as default terminal instead of Alacritty, and refactor scripts and configs

--- .config/hypr/wallpaper_slideshow.sh
+++ .config/hypr/wallpaper_slideshow.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#~/.local/bin/swww-daemon --quiet &
-#sleep 0.2
-#~/.local/bin/swww img `find $HOME/Pictures/wall -type f | shuf -n 1`
-
 while true; do
 	# Change the wallpaper every 5 minutes
 	sleep 5m
