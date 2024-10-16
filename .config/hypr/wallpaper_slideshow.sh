a4379b105b7b7d3e28c7c813bcb0dc8398e4f76a
Diego Vélez <diego.velez.dev@gmail.com>
2024-10-16 07:50:57 -0400

    Add super ultrawide wallpapers and use them as wallpaper slideshow

--- .config/hypr/wallpaper_slideshow.sh
+++ .config/hypr/wallpaper_slideshow.sh
@@ -3,6 +3,6 @@
 while true; do
 	# Change the wallpaper every 5 minutes
 	sleep 5m
-	~/.local/bin/swww img `find $HOME/Pictures/wall -type f | shuf -n 1` --transition-type any --transition-fps 120
+	~/.local/bin/swww img "$(find "$HOME/Pictures/Super Ultrawide Wallpapers/" -type f | shuf -n 1)" --transition-type any --transition-fps 120
 done
 
