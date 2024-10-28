193525b947d4bd22105ee3219a45913fbb73a149
Diego Vélez <diego.velez.dev@gmail.com>
2024-10-27 20:54:46 -0400

    Set up autocmd to setup LSP hover window

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -235,6 +235,24 @@ vim.keymap.set('n', '<leader>rs', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left>
 -- Highlight everything
 vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
 
+-- Show hover with window
+vim.api.nvim_create_autocmd({ 'VimEnter', 'VimResized' }, {
+  desc = 'Setup LSP hover window',
+  callback = function()
+    local width = math.floor(vim.o.columns * 0.8)
+    local height = math.floor(vim.o.lines * 0.3)
+
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = 'rounded',
+      max_width = width,
+      max_height = height,
+    })
+  end,
+})
+
+vim.keymap.set('n', 'h', vim.lsp.buf.hover)
+vim.keymap.set('n', 'K', '<nop>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
