e2df65d5da73230584152d5f5440ab02a467447c
Diego Vélez <diego.velez.dev@gmail.com>
2024-11-19 12:30:23 -0400

    Reformat telescope config to embraze the lazy way

--- .config/nvim/lua/dvt/plugins/telescope.lua
+++ .config/nvim/lua/dvt/plugins/telescope.lua
@@ -22,137 +22,127 @@ return { -- Fuzzy Finder (files, lsp, etc)
     -- Useful for getting pretty icons, but requires a Nerd Font.
     { 'nvim-tree/nvim-web-devicons', enabled = true },
   },
-  config = function()
-    -- Telescope is a fuzzy finder that comes with a lot of different things that
-    -- it can fuzzy find! It's more than just a "file finder", it can search
-    -- many different aspects of Neovim, your workspace, LSP, and more!
-    --
-    -- The easiest way to use Telescope, is to start by doing something like:
-    --  :Telescope help_tags
-    --
-    -- After running this command, a window will open up and you're able to
-    -- type in the prompt window. You'll see a list of `help_tags` options and
-    -- a corresponding preview of the help.
-    --
-    -- Two important keymaps to use while in Telescope are:
-    --  - Insert mode: <c-/>
-    --  - Normal mode: ?
-    --
-    -- This opens a window that shows you all of the keymaps for the current
-    -- Telescope picker. This is really useful to discover what Telescope can
-    -- do as well as how to actually do it!
-
-    -- [[ Configure Telescope ]]
-    -- See `:help telescope` and `:help telescope.setup()`
-    require('telescope').setup {
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
-      --
-      defaults = {
-        layout_strategy = 'center',
-        sorting_strategy = 'ascending',
-        layout_config = {
-          horizontal = {
-            prompt_position = 'top',
-          },
-          center = {
-            width = 0.8,
-            height = 0.3,
-          },
-        },
-        -- Disable all default mappings
-        default_mappings = {},
-        mappings = {
-          i = {
-            -- Basically disable normal mode when using telescope
-            ['<ESC>'] = require('telescope.actions').close,
-            ['<CR>'] = require('telescope.actions').select_default,
-            ['<C-h>'] = require('telescope.actions').select_horizontal,
-            ['<C-v>'] = require('telescope.actions').select_vertical,
-            ['<UP>'] = require('telescope.actions').move_selection_previous,
-            ['<DOWN>'] = require('telescope.actions').move_selection_next,
-            ['<C-u>'] = require('telescope.actions').preview_scrolling_up,
-            ['<C-d>'] = require('telescope.actions').preview_scrolling_down,
-            ['<C-q>'] = require('telescope.actions').send_to_qflist,
-            ['<C-/>'] = require('telescope.actions').which_key,
-          },
-        },
-      },
-      pickers = {
-        help_tags = {
-          mappings = {
-            i = {
-              ['<CR>'] = require('telescope.actions').select_vertical,
-            },
-          },
-        },
-      },
-      extensions = {
-        ['ui-select'] = {
-          require('telescope.themes').get_dropdown(),
-        },
-      },
-    }
-
+  init = function()
+    vim.g.is_inside_work_tree = {}
     -- Enable Telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
-
-    -- See `:help telescope.builtin`
-    local builtin = require 'telescope.builtin'
-
-    -- Search git files if within git project, otherwise do entire system
-    -- Gotten from: https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#falling-back-to-find_files-if-git_files-cant-find-a-git-directory
-    --
-    -- Cache cause this shit expensive dawg
-    local is_inside_work_tree = {}
-
-    local project_files = function()
-      local cwd = vim.fn.getcwd()
-      if is_inside_work_tree[cwd] == nil then
-        vim.fn.system 'git rev-parse --is-inside-work-tree'
-        is_inside_work_tree[cwd] = vim.v.shell_error == 0
-      end
-
-      if is_inside_work_tree[cwd] then
-        builtin.git_files { no_ignore = true, no_ignore_parent = true, hidden = true }
-      else
-        builtin.find_files { no_ignore = true, no_ignore_parent = true, hidden = true }
-      end
-    end
-
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', project_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>so', builtin.oldfiles, { desc = '[S]earch [O]ld Files' })
-    vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch Existing [B]uffers' })
-
-    -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' })
-
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
-    vim.keymap.set('n', '<leader>s/', function()
-      builtin.live_grep {
-        grep_open_files = true,
-        prompt_title = 'Live Grep in Open Files',
-      }
-    end, { desc = '[S]earch [/] in Open Files' })
-
-    -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set('n', '<leader>sc', function()
-      builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[S]earch [C]onfig files' })
   end,
+  keys = {
+    {
+      -- It's also possible to pass additional configuration options.
+      --  See `:help telescope.builtin.live_grep()` for information about particular keys
+      '<leader>s/',
+      function()
+        require('telescope.builtin').live_grep {
+          grep_open_files = true,
+          prompt_title = 'Live Grep in Open Files',
+        }
+      end,
+      desc = '[S]earch [/] in Open Files',
+    },
+    {
+      -- Shortcut for searching your Neovim configuration files
+      '<leader>sc',
+      function()
+        require('telescope.builtin').find_files { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = '[S]earch [C]onfig files',
+    },
+    {
+      -- Slightly advanced example of overriding default behavior and theme
+      '<leader>/',
+      function()
+        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end,
+      desc = '[/] Fuzzily search in current buffer',
+    },
+
+    { '<leader>sb', require('telescope.builtin').buffers, desc = '[S]earch Existing [B]uffers' },
+    { '<leader>so', require('telescope.builtin').oldfiles, desc = '[S]earch [O]ld Files' },
+    { '<leader>sr', require('telescope.builtin').resume, desc = '[S]earch [R]esume' },
+    { '<leader>sd', require('telescope.builtin').diagnostics, desc = '[S]earch [D]iagnostics' },
+    { '<leader>sg', require('telescope.builtin').live_grep, desc = '[S]earch by [G]rep' },
+    { '<leader>sw', require('telescope.builtin').grep_string, desc = '[S]earch current [W]ord' },
+    { '<leader>ss', require('telescope.builtin').builtin, desc = '[S]earch [S]elect Telescope' },
+    {
+      -- Search git files if within git project, otherwise do entire system
+      -- Gotten from: https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#falling-back-to-find_files-if-git_files-cant-find-a-git-directory
+      '<leader>sf',
+      function()
+        local cwd = vim.fn.getcwd()
+        if vim.g.is_inside_work_tree[cwd] == nil then
+          vim.fn.system 'git rev-parse --is-inside-work-tree'
+          vim.g.is_inside_work_tree[cwd] = vim.v.shell_error == 0
+        end
+
+        if vim.g.is_inside_work_tree[cwd] then
+          require('telescope.builtin').git_files { no_ignore = true, no_ignore_parent = true, hidden = true }
+        else
+          require('telescope.builtin').find_files { no_ignore = true, no_ignore_parent = true, hidden = true }
+        end
+      end,
+      desc = '[S]earch [F]iles',
+    },
+    {
+      '<leader>sk',
+      require('telescope.builtin').keymaps,
+      desc = '[S]earch [K]eymaps',
+    },
+    {
+      '<leader>sh',
+      require('telescope.builtin').help_tags,
+      desc = '[S]earch [H]elp',
+    },
+  },
+  opts = {
+    defaults = {
+      layout_strategy = 'center',
+      sorting_strategy = 'ascending',
+      layout_config = {
+        horizontal = {
+          prompt_position = 'top',
+        },
+        center = {
+          width = 0.8,
+          height = 0.3,
+        },
+      },
+      -- Disable all default mappings
+      default_mappings = {},
+      mappings = {
+        i = {
+          -- Basically disable normal mode when using telescope
+          ['<ESC>'] = require('telescope.actions').close,
+          ['<CR>'] = require('telescope.actions').select_default,
+          ['<C-h>'] = require('telescope.actions').select_horizontal,
+          ['<C-v>'] = require('telescope.actions').select_vertical,
+          ['<UP>'] = require('telescope.actions').move_selection_previous,
+          ['<DOWN>'] = require('telescope.actions').move_selection_next,
+          ['<C-u>'] = require('telescope.actions').preview_scrolling_up,
+          ['<C-d>'] = require('telescope.actions').preview_scrolling_down,
+          ['<C-q>'] = require('telescope.actions').send_to_qflist,
+          ['<C-/>'] = require('telescope.actions').which_key,
+        },
+      },
+    },
+    pickers = {
+      help_tags = {
+        mappings = {
+          i = {
+            ['<CR>'] = require('telescope.actions').select_vertical,
+          },
+        },
+      },
+    },
+    extensions = {
+      ['ui-select'] = {
+        require('telescope.themes').get_dropdown(),
+      },
+    },
+  },
 }
