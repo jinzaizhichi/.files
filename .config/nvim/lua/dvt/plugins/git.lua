fc264d7418b74173b52bc6b2f198a3fedd2110d5
Diego Vélez <diego.velez.dev@gmail.com>
2024-11-24 09:09:22 -0400

    Consolidate all Git plugins into one file

---
+++ .config/nvim/lua/dvt/plugins/git.lua
@@ -0,0 +1,80 @@
+return {
+  {
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      current_line_blame_opts = {
+        delay = 0,
+      },
+      preview_config = {
+        border = 'rounded',
+      },
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Hunk Navigation
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Jump to previous Git [c]hange' })
+
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Jump to next Git [c]hange' })
+
+        -- Hunk Actions
+        map('n', '<leader>gs', gitsigns.stage_hunk, { desc = '[S]tage hunk' })
+        map('v', '<leader>gs', function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = '[S]tage hunk' })
+        map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = '[U]nstage previous staged hunk' })
+        map('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[R]eset hunk' })
+        map('v', '<leader>gr', function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = '[R]eset hunk' })
+        map('n', '<leader>gp', gitsigns.preview_hunk, { desc = '[P]review hunk' })
+
+        -- Buffer Actions
+        map('n', '<leader>gS', gitsigns.stage_buffer, { desc = '[S]tage buffer' })
+        map('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[R]eset buffer' })
+
+        -- Diffing
+        map('n', '<leader>gd', gitsigns.diffthis, { desc = '[D]iff against head' })
+        map('n', '<leader>gD', function()
+          gitsigns.diffthis '~'
+        end, { desc = '[D]iff against previous commit' })
+
+        -- Blame
+        map('n', '<leader>gb', gitsigns.toggle_current_line_blame, { desc = 'Toggle [b]lame' })
+        map('n', '<leader>gB', function()
+          gitsigns.blame_line { full = true }
+        end, { desc = 'Show [b]lame' })
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Git [h]unk' })
+      end,
+    },
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      { '<leader>gl', ':LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+}
