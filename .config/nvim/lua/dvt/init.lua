c03158efa9e655d13d1333366a33b51f92adca72
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-04 12:22:33 -0400

    Setup git variables when using nvim from a directory tracked by my dotfiles repo

--- .config/nvim/lua/dvt/init.lua
+++ .config/nvim/lua/dvt/init.lua
@@ -29,3 +29,24 @@ vim.diagnostic.config {
   float = { border = 'rounded' },
   signs = { text = diagnostic_signs },
 }
+
+-- Setup git variables if working on a directory tracked by my dotfiles repo
+local home = vim.fn.getenv 'HOME'
+local git_dir = home .. '/.files'
+local work_tree = home
+local dotfiles_files = vim
+  .system({
+    'git',
+    '--git-dir=' .. git_dir,
+    '--work-tree=' .. work_tree,
+    'ls-files',
+    '--full-name',
+    '/home/dvt',
+  }, { text = true })
+  :wait()
+local cwd = vim.fn.getcwd()
+cwd = string.sub(cwd, #home + 2)
+if cwd ~= nil and string.find(dotfiles_files.stdout, cwd) ~= nil then
+  vim.env.GIT_WORK_TREE = '/home/dvt'
+  vim.env.GIT_DIR = '/home/dvt/.files'
+end
