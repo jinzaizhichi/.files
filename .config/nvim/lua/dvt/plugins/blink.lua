85cd29d3c26007de0cd9f62874b4c72a23405ec3
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-05 17:44:40 -0400

    Add blink to replace cmp

---
+++ .config/nvim/lua/dvt/plugins/blink.lua
@@ -0,0 +1,46 @@
+return {
+  'saghen/blink.cmp',
+  lazy = false, -- lazy loading handled internally
+  dependencies = 'rafamadriz/friendly-snippets',
+  version = 'v0.*',
+  opts = {
+    keymap = {
+      ['<C-CR>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<C-h>'] = { 'hide' },
+      ['<CR>'] = { 'select_and_accept', 'fallback' },
+      ['<UP>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+      ['<DOWN>'] = { 'select_next', 'snippet_forward', 'fallback' },
+      ['C-u'] = { 'scroll_documentation_up', 'fallback' },
+      ['C-d'] = { 'scroll_documentation_down', 'fallback' },
+    },
+
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono',
+    },
+
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, via `opts_extend`
+    sources = {
+      completion = {
+        enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+    },
+
+    -- experimental auto-brackets support
+    -- completion = { accept = { auto_brackets = { enabled = true } } }
+
+    -- experimental signature help support
+    signature = {
+      enabled = true,
+    },
+  },
+  -- allows extending the enabled_providers array elsewhere in your config
+  -- without having to redefine it
+  opts_extend = { 'sources.completion.enabled_providers' },
+}
