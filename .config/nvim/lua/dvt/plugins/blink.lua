ddfb56af9c2ab31454006f48009d518d5aa4a20c
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-06 10:51:14 -0400

    Enable auto showing documentation window for completion item

--- .config/nvim/lua/dvt/plugins/blink.lua
+++ .config/nvim/lua/dvt/plugins/blink.lua
@@ -3,6 +3,9 @@ return {
   lazy = false, -- lazy loading handled internally
   dependencies = 'rafamadriz/friendly-snippets',
   version = 'v0.*',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
   opts = {
     keymap = {
       ['<C-CR>'] = { 'show', 'show_documentation', 'hide_documentation' },
@@ -12,6 +15,14 @@ return {
       ['<DOWN>'] = { 'select_next', 'snippet_forward', 'fallback' },
       ['C-u'] = { 'scroll_documentation_up', 'fallback' },
       ['C-d'] = { 'scroll_documentation_down', 'fallback' },
+    },
+
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 0,
+        update_delay_ms = 0,
+      },
     },
 
     appearance = {
