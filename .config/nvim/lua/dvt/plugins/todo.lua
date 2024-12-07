86aab1c720035b24019f775493df87f84a11d9f3
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-07 19:28:14 -0400

    Add search todo keymap

--- .config/nvim/lua/dvt/plugins/todo.lua
+++ .config/nvim/lua/dvt/plugins/todo.lua
@@ -3,5 +3,26 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    {
+      '[t',
+      function()
+        require('todo-comments').jump_prev()
+      end,
+      desc = 'Previous [t]odo comment',
+    },
+    {
+      ']t',
+      function()
+        require('todo-comments').jump_next()
+      end,
+      desc = 'Next [t]odo comment',
+    },
+    {
+      '<leader>st',
+      ':TodoTelescope<CR>',
+      desc = 'Search [T]odo',
+    },
+  },
   opts = { signs = true },
 }
