be612cf3ea68086f6b9d0071b36c158adf8b7d1e
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-07 21:18:41 -0400

    Move treesitter-context to the treesitter plugin file

--- .config/nvim/lua/dvt/plugins/context.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'nvim-treesitter/nvim-treesitter-context',
-  opts = {
-    max_lines = 2,
-    multiline_threshold = 1,
-    trim_scope = 'inner',
-  },
-}
--- .config/nvim/lua/dvt/plugins/treesitter.lua
+++ .config/nvim/lua/dvt/plugins/treesitter.lua
@@ -1,82 +1,116 @@
 return { -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  version = false,
-  build = ':TSUpdate',
-  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-  init = function(plugin)
-    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-    -- Luckily, the only things that those plugins need are the custom queries, which we make available
-    -- during startup.
-    require('lazy.core.loader').add_to_rtp(plugin)
-    require 'nvim-treesitter.query_predicates'
-  end,
-  ---@type TSConfig
-  ---@diagnostic disable-next-line: missing-fields
-  opts = {
-    ensure_installed = {
-      'bash',
-      'c',
-      'diff',
-      'go',
-      'html',
-      'lua',
-      'luadoc',
-      'markdown',
-      'markdown_inline',
-      'python',
-      'query',
-      'vim',
-      'vimdoc',
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-    textobjects = {
-      move = {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    version = false,
+    build = ':TSUpdate',
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require('lazy.core.loader').add_to_rtp(plugin)
+      require 'nvim-treesitter.query_predicates'
+    end,
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'go',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'query',
+        'vim',
+        'vimdoc',
+      },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
         enable = true,
-        goto_next_start = {
-          [']f'] = '@function.outer',
-          [']c'] = '@class.outer',
-          [']a'] = '@parameter.inner',
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = {
+            [']f'] = '@function.outer',
+            [']c'] = '@class.outer',
+            [']a'] = '@parameter.inner',
+          },
+          -- goto_next_end = {
+          --   [']F'] = '@function.outer',
+          --   [']C'] = '@class.outer',
+          --   [']A'] = '@parameter.inner',
+          -- },
+          goto_previous_start = {
+            ['[f'] = '@function.outer',
+            ['[c'] = '@class.outer',
+            ['[a'] = '@parameter.inner',
+          },
+          -- goto_previous_end = {
+          --   ['[F'] = '@function.outer',
+          --   ['[C'] = '@class.outer',
+          --   ['[A'] = '@parameter.inner',
+          -- },
         },
-        -- goto_next_end = {
-        --   [']F'] = '@function.outer',
-        --   [']C'] = '@class.outer',
-        --   [']A'] = '@parameter.inner',
-        -- },
-        goto_previous_start = {
-          ['[f'] = '@function.outer',
-          ['[c'] = '@class.outer',
-          ['[a'] = '@parameter.inner',
-        },
-        -- goto_previous_end = {
-        --   ['[F'] = '@function.outer',
-        --   ['[C'] = '@class.outer',
-        --   ['[A'] = '@parameter.inner',
-        -- },
       },
     },
+    config = function(_, opts)
+      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup(opts)
+
+      -- There are additional nvim-treesitter modules that you can use to interact
+      -- with nvim-treesitter. You should go explore a few and see what interests you:
+      --
+      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    end,
   },
-  config = function(_, opts)
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-    ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup(opts)
-
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  end,
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'BufEnter',
+    keys = {
+      {
+        '<leader>tc',
+        function()
+          local tsc = require 'treesitter-context'
+          tsc.toggle()
+          if tsc.enabled() then
+            require('fidget').notify(
+              'Context enabled',
+              nil,
+              { key = 'toggle_context', annote = 'toogle' }
+            )
+          else
+            require('fidget').notify(
+              'Context disabled',
+              nil,
+              { key = 'toggle_context', annote = 'toogle' }
+            )
+          end
+        end,
+        desc = 'Toggle [c]ontext',
+      },
+    },
+    opts = {
+      max_lines = 2,
+      multiline_threshold = 1,
+      trim_scope = 'inner',
+    },
+  },
 }
