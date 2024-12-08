45c02c89432039e9fe4bcb5cbd943492fbfa0d49
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-07 20:58:39 -0400

    Setup lazydev for blink and wezterm types

--- .config/nvim/lua/dvt/plugins/blink.lua
+++ .config/nvim/lua/dvt/plugins/blink.lua
@@ -1,57 +1,137 @@
 return {
-  'saghen/blink.cmp',
-  lazy = false, -- lazy loading handled internally
-  dependencies = 'rafamadriz/friendly-snippets',
-  version = 'v0.*',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = {
-      ['<C-CR>'] = { 'show', 'show_documentation', 'hide_documentation' },
-      ['<C-h>'] = { 'hide' },
-      ['<CR>'] = { 'select_and_accept', 'fallback' },
-      ['<UP>'] = { 'select_prev', 'snippet_backward', 'fallback' },
-      ['<DOWN>'] = { 'select_next', 'snippet_forward', 'fallback' },
-      ['C-u'] = { 'scroll_documentation_up', 'fallback' },
-      ['C-d'] = { 'scroll_documentation_down', 'fallback' },
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    dependencies = {
+      { 'justinsgithub/wezterm-types', lazy = true },
+      { 'Bilal2453/luvit-meta', lazy = true },
     },
-
-    completion = {
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 0,
-        update_delay_ms = 0,
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        { path = 'wezterm-types', mods = { 'wezterm' } },
       },
-    },
-
-    appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono',
-    },
-
-    -- default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, via `opts_extend`
-    sources = {
-      completion = {
-        enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-    },
-
-    -- experimental auto-brackets support
-    -- completion = { accept = { auto_brackets = { enabled = true } } }
-
-    -- experimental signature help support
-    signature = {
-      enabled = true,
     },
   },
-  -- allows extending the enabled_providers array elsewhere in your config
-  -- without having to redefine it
-  opts_extend = { 'sources.completion.enabled_providers' },
+  {
+    'saghen/blink.cmp',
+    lazy = false, -- lazy loading handled internally
+    dependencies = 'rafamadriz/friendly-snippets',
+    version = 'v0.*',
+    event = 'InsertEnter',
+    -- allows extending the enabled_providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { 'sources.completion.enabled_providers' },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        ['<C-CR>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-h>'] = { 'hide' },
+        ['<CR>'] = { 'select_and_accept', 'fallback' },
+        ['<UP>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+        ['<DOWN>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['C-u'] = { 'scroll_documentation_up', 'fallback' },
+        ['C-d'] = { 'scroll_documentation_down', 'fallback' },
+      },
+
+      completion = {
+        menu = {
+          draw = {
+            treesitter = true,
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 0,
+          update_delay_ms = 0,
+        },
+      },
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono',
+      },
+
+      -- default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, via `opts_extend`
+      -- sources = {
+      --   completion = {
+      --     enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+      --   },
+      --   providers = {
+      --     lsp = {
+      --       fallback_for = { 'lazydev' },
+      --     },
+      --     lazydev = {
+      --       name = 'LazyDev',
+      --       module = 'lazydev.integrations.blink',
+      --     },
+      --   },
+      -- },
+      sources = {
+        -- add lazydev to your completion providers
+        completion = {
+          enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+        },
+        providers = {
+          -- dont show LuaLS require statements when lazydev has items
+          lsp = { fallback_for = { 'lazydev' } },
+          lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink' },
+        },
+      },
+
+      -- experimental auto-brackets support
+      -- completion = { accept = { auto_brackets = { enabled = true } } }
+
+      -- experimental signature help support
+      signature = {
+        enabled = true,
+      },
+    },
+    ---@param opts blink.cmp.Config
+    config = function(_, opts)
+      -- setup compat sources
+      local enabled = opts.sources.completion.enabled_providers
+      for _, source in ipairs(opts.sources.compat or {}) do
+        opts.sources.providers[source] = vim.tbl_deep_extend(
+          'force',
+          { name = source, module = 'blink.compat.source' },
+          opts.sources.providers[source] or {}
+        )
+        if type(enabled) == 'table' and not vim.tbl_contains(enabled, source) then
+          table.insert(enabled, source)
+        end
+      end
+
+      -- check if we need to override symbol kinds
+      for _, provider in pairs(opts.sources.providers or {}) do
+        ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+        if provider.kind then
+          require('blink.cmp.types').CompletionItemKind[provider.kind] = provider.kind
+          ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+          local transform_items = provider.transform_items
+          ---@param ctx blink.cmp.Context
+          ---@param items blink.cmp.CompletionItem[]
+          provider.transform_items = function(ctx, items)
+            items = transform_items and transform_items(ctx, items) or items
+            for _, item in ipairs(items) do
+              item.kind = provider.kind or item.kind
+            end
+            return items
+          end
+        end
+      end
+
+      require('blink.cmp').setup(opts)
+    end,
+  },
 }
