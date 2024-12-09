27544602feb449e0658d7a0719ebbb45d5a6f34b
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-09 06:29:41 -0400

    Fix blink.cmp documentation keymaps, and use blink highlight groups

--- .config/nvim/lua/dvt/plugins/blink.lua
+++ .config/nvim/lua/dvt/plugins/blink.lua
@@ -30,12 +30,12 @@ return {
     opts = {
       keymap = {
         ['<C-CR>'] = { 'show', 'show_documentation', 'hide_documentation' },
-        ['<C-h>'] = { 'hide' },
+        ['<C-e>'] = { 'hide' },
         ['<CR>'] = { 'select_and_accept', 'fallback' },
         ['<UP>'] = { 'select_prev', 'snippet_backward', 'fallback' },
         ['<DOWN>'] = { 'select_next', 'snippet_forward', 'fallback' },
-        ['C-u'] = { 'scroll_documentation_up', 'fallback' },
-        ['C-d'] = { 'scroll_documentation_down', 'fallback' },
+        ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
       },
 
       completion = {
@@ -55,7 +55,7 @@ return {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
         -- Useful for when your theme doesn't support blink.cmp
         -- will be removed in a future release
-        use_nvim_cmp_as_default = true,
+        use_nvim_cmp_as_default = false,
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
